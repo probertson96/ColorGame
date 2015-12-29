c11ad6574eadcd98244aac986ce8e34b24d87677
Patrick Robertson <Patrick@Patricks-MacBook-Pro.local>
2015-12-29 18:45:51 +1100

    App now randomises which color is selected. Next step is to randomise colors

--- colorGame.js
+++ colorGame.js
@@ -7,8 +7,9 @@ var colors = [
 	"rgb(0, 255, 0)"
 ];
 
+var messageDisplay = document.getElementById("message")
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var titleSpan = document.getElementById("titleSpan");
 titleSpan.textContent = pickedColor;
 
@@ -23,10 +24,25 @@ for (var i = 0; i < squares.length; i++) {
 		console.log(clickedColor);
 		console.log(pickedColor);
 		if (clickedColor === pickedColor) {
-			alert("Correct");
+			messageDisplay.innerHTML = "Success";
+			changeColors(clickedColor);
 
 		} else {
-			alert("Wrong");
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
 	});
+}
+
+function changeColors(color) {
+	//Loop through squares
+	for (var i = 0; i < squares.length; i++) {
+		//Change color to match given color
+		squares[i].style.background = color;
+	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
 }
