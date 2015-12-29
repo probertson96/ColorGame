0aa1de103e6b36464d64b0834d0f82aa2e114fca
Patrick Robertson <Patrick@Patricks-MacBook-Pro.local>
2015-12-29 18:25:06 +1100

    Click events and logic added to compare clicked rgb value and the correct rgb value

--- colorGame.js
+++ colorGame.js
@@ -1,14 +1,32 @@
 var colors = [
-	"rgb(255,0,0)",
-	"rgb(255,255,0)",
-	"rgb(0,255,255)",
-	"rgb(255,0,255)",
-	"rgb(0,0,255)",
-	"rgb(0,255,0)"
+	"rgb(255, 0, 0)",
+	"rgb(255, 255, 0)",
+	"rgb(0, 255, 255)",
+	"rgb(255, 0, 255)",
+	"rgb(0, 0, 255)",
+	"rgb(0, 255, 0)"
 ];
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var titleSpan = document.getElementById("titleSpan");
+titleSpan.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
+	//Add initial colors to squares
 	squares[i].style.background = colors[i];
+
+	//Add click listeners to squares
+	squares[i].addEventListener("click", function () {
+		//Grab color of clicked square, compare to pickedColor
+		var clickedColor = this.style.background;
+		console.log(clickedColor);
+		console.log(pickedColor);
+		if (clickedColor === pickedColor) {
+			alert("Correct");
+
+		} else {
+			alert("Wrong");
+		}
+	});
 }
