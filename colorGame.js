4a80164041a3cae961dd41560a9f03f449f77048
Patrick Robertson <Patrick@Patricks-MacBook-Pro.local>
2015-12-30 07:31:39 +1100

    Random colors now generated and applied to squares

--- colorGame.js
+++ colorGame.js
@@ -1,17 +1,11 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(255, 0, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(0, 255, 0)"
-];
+var colors = generateRandomColors(6);
 
 var messageDisplay = document.getElementById("message")
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var titleSpan = document.getElementById("titleSpan");
 titleSpan.textContent = pickedColor;
+var h1 = document.querySelector("h1");
 
 for (var i = 0; i < squares.length; i++) {
 	//Add initial colors to squares
@@ -26,6 +20,7 @@ for (var i = 0; i < squares.length; i++) {
 		if (clickedColor === pickedColor) {
 			messageDisplay.innerHTML = "Success";
 			changeColors(clickedColor);
+			h1.style.background = clickedColor;
 
 		} else {
 			this.style.background = "#232323";
@@ -45,4 +40,25 @@ function changeColors(color) {
 function pickColor() {
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num) {
+	//Make an array
+	var arr = [];
+	//Add num random colors to array
+	for (var i = 0; i < num; i++) {
+		//Get random color and push into array
+		arr.push(randomColor());
+
+	}
+	//Return array
+	return arr;
+}
+
+function randomColor() {
+	//Pick a "red" from 0 - 255, "green" from 0 - 255, "blue" from 0 - 255
+	var r = Math.floor(Math.random() * 256)
+	var g = Math.floor(Math.random() * 256)
+	var b = Math.floor(Math.random() * 256)
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
